c4d7396107b78d69cc2c16f5483b8393a69427d2
TrucDangMinh <minhtrucdang3012@gmail.com>
2024-11-11 13:50:59 +0000

    Lấy xpath

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def get_driver():
   options.add_argument("disable-dev-shm-usage")
   options.add_argument("no-sandbox")
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
-  options.add_argument("disable-blink-features=AutomationControlled"")
-  
+  options.add_argument("disable-blink-features=AutomationControlled")
+
   driver = webdriver.Chrome(options=options)
   driver.get("http://automated.pythonanywhere.com")
   return driver
 
 def main():
   driver = get_driver()
+  #Điền link xpath vào value
   element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
-  return element.text
+  return element
+
+print(main())
